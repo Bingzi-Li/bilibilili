6b0abfb9f172ef51c087a384d0996b35e0f580dc
FrankLeeeee <31818963+FrankLeeeee@users.noreply.github.com>
2019-09-16 23:24:07 +0800

    fixed log out bugs

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -87,7 +87,11 @@ module.exports = function(app, passport) {
                 });
             }
         })
+    })
 
-
+    app.get('/logout', (req, res) => {
+        req.session.destroy(function(err) {
+            res.redirect('/login');
+        });
     })
 }
